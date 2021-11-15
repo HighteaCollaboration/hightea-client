7e951709571883f8c867171076cfe8eef99ad58c
Andrei Popescu <adp58@cam.ac.uk>
2021-11-15 09:34:08 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_namespace_packages
+from src.hightea.client import __version__
+
+setup(
+    name = "hightea-client",
+    version = __version__,
+    author = "Hightea Collaboration",
+    author_email = "hightea@hep.phy.cam.ac.uk",
+    url = "https://github.com/HighteaCollaboration/hightea-client",
+    description = "Command line interface to hightea project",
+    license = "MIT",
+    packages=find_namespace_packages(
+        where='src',
+        # include=[
+        #     'hightea.client.*'
+        #     ]
+        ),
+    package_dir={
+        '': 'src',
+    },
+    scripts=['./bin/highteacli'],
+    install_requires = [
+        "numpy",
+        "requests",
+        "urllib3",
+    ],
+    python_requires = ">=3.6",
+)

d884a64264bfce940e00110acd91116c1c57c883
Rene Poncelet <re.poncelet@gmail.com>
2021-11-16 16:37:53 +0000

    test

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
-    'recommonmark',
+    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -99,7 +99,7 @@ autodoc_member_order = 'bysource'
 # https://github.com/readthedocs/recommonmark/blob/ddd56e7717e9745f11300059e4268e204138a6b1/docs/conf.py
 # app setup hook
 def setup(app):
-    app.add_config_value('recommonmark_config', {
-        'enable_eval_rst': True,
-    }, True)
+#    app.add_config_value('recommonmark_config', {
+#        'enable_eval_rst': True,
+#    }, True)
     app.add_transform(AutoStructify)

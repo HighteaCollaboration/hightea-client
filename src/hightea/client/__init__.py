70ace89a1a35238f99ead9e7e1f9a991eb5eba15
Rene Poncelet <poncelet@hep.phy.cam.ac.uk>
2023-04-06 14:37:05 +0100

    version bump to 0.5.5

--- src/hightea/client/__init__.py
+++ src/hightea/client/__init__.py
@@ -3,7 +3,7 @@ A command line interface for the high energy theory database.
 """
 
 
-__version__ = '0.5.4'
+__version__ = '0.5.5'
 
 from .interface import Interface
 from .datahandler import DataHandler
